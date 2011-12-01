213d1f800de0421047e211382c90594a4baf041c
busticated <itsbusticated@gmail.com>
2011-11-30 21:22:00 -0800

    Add chz release tagger script, add release alias to hgrc

---
+++ utils/chz-releasetagger.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+set -e
+
+# make sure we're in a repo
+repoRoot=$(hg root)
+if [ -z $repoRoot ]
+then
+	exit
+fi
+
+constantsFile=$repoRoot/wp-shared/constants.inc.php
+styleFile=$repoRoot/style.css
+
+buildTimestamp(){
+	year=$(date +%Y)
+	month=$(date +%m)
+	day=$(date +%d)
+
+	echo "$year.$month.$day"
+}
+
+getLastPublished(){
+	regx="define\( \'THEME_VERSION\', \'(.+)\' \)"
+
+	while read LINE
+	do
+		[[ $LINE =~ $regx ]]
+		if [ ! -z "${BASH_REMATCH[1]}" ]
+		then
+			pubDate="${BASH_REMATCH[1]}"
+		fi
+	done < $constantsFile
+	echo $pubDate
+}
+
+setReleaseTag(){
+	origDate=$(getLastPublished | sed -e 's/\([0-9]*.[0-9]*.[0-9]*\).*/\1/')
+	origVer=$(getLastPublished | sed -e 's/...........\(.*\)/\1/')
+	newDate=$(buildTimestamp | sed -e 's/\([0-9]*.[0-9]*.[0-9]*\).*/\1/')
+
+	if [ "$origDate" == "$newDate" ]
+	then
+		echo $newDate.$((origVer+1))
+	else
+		echo $newDate.1
+	fi
+}
+
+relTag=$(setReleaseTag)
+hgTag='release_'$( echo $relTag | sed -e 's/\([0-9]\{4\}\).\([0-9]\{1,2\}\).\([0-9]\{1,2\}.[0-9]\{1,2\}\)/\1\2\3/')
+
+echo "::enter your release details below::"
+read msg
+echo "::release tag preview: hg tag -m" \"$msg\" $hgTag
+read -p "::is this good to go (y/n)? "
+if [ "$REPLY" == "n" ]
+then
+	echo "::...aborting release (╯°□°）╯︵ ┻━┻ "
+	exit
+fi
+
+# find and replace release tags
+echo "::...updating theme version"
+sed -i '' -e "s/\(VERSION....\)[0-9]\{4\}.[0-9]\{1,2\}.[0-9]\{1,2\}.[0-9]\{1,2\}/\1$relTag/" $constantsFile
+sed -i '' -e "s/\(Version:.\)[0-9]\{4\}.[0-9]\{1,2\}.[0-9]\{1,2\}.[0-9]\{1,2\}/\1$relTag/" $styleFile
+
+echo "::...committing changeset"
+hg com -m "Update release version"
+
+echo "::...tagging revision"
+hg tag -m "${msg}" ${hgTag}
+
+echo "::...pushing"
+hg push
