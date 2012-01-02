050ec6bd37e78df3c9f7ce8efdbd9c6d9d4507e0
busticated <itsbusticated@gmail.com>
2012-01-02 13:40:24 -0800

    Use separate gitconfig for winders, update winders setup to use new gitconfig as well as global git ignore file

--- setup.ps1
+++ setup.ps1
@@ -3,4 +3,5 @@ function mklink { cmd /c mklink $args }
 
 mklink /H "$HOME\Documents\WindowsPowerShell\profile.ps1" "C:\dev\dotfilez\profile.ps1"
 mklink /H "$HOME\Mercurial.ini" "C:\dev\dotfilez\Mercurial.ini"
-mklink /H "$HOME\.gitconfig" "C:\dev\dotfilez\gitconfig" #excludes file path needs fixin'
+mklink /H "$HOME\.gitconfig" "C:\dev\dotfilez\gitconfig-windows"
+mklink /H "$HOME\.gitignore_global" "C:\dev\dotfilez\gitignore_global"
