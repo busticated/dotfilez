64d72a0fa1b79aec4aa91bf5874ac14551c5116d
busticated <itsbusticated@gmail.com>
2017-11-04 15:49:26 -0700

    add homebrew to setup script, improve logging

--- setup.sh
+++ setup.sh
@@ -28,9 +28,24 @@ mkdir -p ~/.vim/tmp
 
 # grab vundle if needed
 if [ ! -d "$HOME/.vim/bundle/vundle" ]; then
+    echo ":::: Installing vim vundle plugin manager..."
     git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+else
+    echo ":::: vim vundle already installed. Skipping..."
+fi
+
+# install brew and brewfile bundles
+if [[ $(command -v brew) == "" ]]; then
+    echo "Installing homebrew package manager..."
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    brew tap homebrew/bundle
+    brew bundle
+else
+    echo ":::: Updating homebrew packages..."
+    brew update
 fi
 
 # setup symlinks for user config files
+echo ":::: Linking config files..."
 linkUserConfigFiles ${files[@]}
 exit
